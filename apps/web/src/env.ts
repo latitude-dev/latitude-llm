4d053dc23fd751baf13dfacd26920080772beb11
Gerard Clos <clos.gerard@gmail.com>
2024-07-01 16:07:25 +0200

    feature: api scaffolding

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -5,9 +5,15 @@ export default createEnv({
   server: {
     DATABASE_URL: z.string(),
     TEST_DATABASE_URL: z.string(),
+    API_KEY: z.string(),
+    ELASTIC_USERNAME: z.string(),
+    ELASTIC_PASSWORD: z.string(),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+    API_KEY: process.env.API_KEY,
+    ELASTIC_USERNAME: process.env.ELASTIC_USERNAME,
+    ELASTIC_PASSWORD: process.env.ELASTIC_PASSWORD
   },
 })
---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import env from './env'
+
+export function middleware(req: NextRequest) {
+  const header = req.headers.get('Authorization')
+  const key = header?.match(/^Bearer (.*)$/)?.[1]
+  if (key !== env.API_KEY)
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/api/:path*',
+}
