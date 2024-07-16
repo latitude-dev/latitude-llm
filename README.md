b270e04be87bfde0101a2edbe4a5da31b4686c32
Gerard Clos <clos.gerard@gmail.com>
2024-07-16 13:06:19 +0200

    removed more docker fat and added studio to tmuxinator

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
 # Latitude LLM
-
-# How to build Dockerimages?
-
-Usually you just need to run `docker compose up --build` and it will work. But
-if you're updating the production build of an app you need to change the [target](https://docs.docker.com/compose/compose-file/build/#target) in the docker compose app you want to update
-
-## Drizzle Studio
-
-You can open a DB UI with the following command:
-
-```bash
-docker compose exec web pnpm:db:studio
-```
-
-And then IMPORTANT: change host from `0.0.0.0` to `localhost`
-https://local.drizzle.studio/?host=localhost
--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,21 +1,17 @@
 import dotenv, { type DotenvPopulateInput } from 'dotenv'
 
 const env = process.env.NODE_ENV || 'development'
-const useLocalhost = process.env.USE_LOCALHOST === 'true'
 
 // Don't write production .env files!
 if (env !== 'production') {
-  const dbHost = useLocalhost ? `localhost` : `db`
-  const redisHost = useLocalhost ? `localhost` : `redis`
-
   dotenv.populate(process.env as DotenvPopulateInput, {
     NODE_ENV: env,
-    DATABASE_URL: `postgres://latitude:secret@${dbHost}:5432/latitude_development`,
+    DATABASE_URL: `postgres://latitude:secret@localhost:5432/latitude_development`,
     ELASTIC_URL: 'https://elastic:9200',
     ELASTIC_USERNAME: 'latitude',
     ELASTIC_PASSWORD: 'secret',
     REDIS_PORT: '6379',
-    REDIS_HOST: redisHost,
+    REDIS_HOST: 'localhost',
     REDIS_PASSWORD: 'secret',
   })
 }
