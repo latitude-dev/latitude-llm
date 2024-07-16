01d73d0c75e3b7f4f513d8326e8ef396749780fe
Gerard Clos <clos.gerard@gmail.com>
2024-07-16 12:39:54 +0200

    check for node and pnpm in start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+# If node exists and is version is lower than 20.x, raise an error and exit
+if command -v node &> /dev/null
+then
+    # Get the node version and remove the 'v' character
+    node_version=$(node -v | sed 's/^v//')
+    
+    # Extract the major version number
+    major_version=$(echo "$node_version" | cut -d. -f1)
+    
+    # Check if the major version number is less than 20
+    if [ "$major_version" -lt 20 ]
+    then
+        echo "Node version is lower than 20.x. Please update your node version."
+        exit 1
+    fi
+else
+    echo "Node.js is not installed. Please install Node.js."
+    exit 1
+fi
+
+# Install pnpm
+if ! command -v pnpm &> /dev/null
+then
+  npm install -g pnpm 
+fi
+
 # Install tmux 
 if ! command -v tmux &> /dev/null
 then
