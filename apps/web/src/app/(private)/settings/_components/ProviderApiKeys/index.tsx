7680ee0604aa2626b77d09f8e211a150295fe8b0
Gerard Clos <clos.gerard@gmail.com>
2024-07-30 14:50:46 +0200

    minor: blankslate for provider apikeys

--- apps/web/src/app/(private)/settings/_components/ProviderApiKeys/index.tsx
+++ apps/web/src/app/(private)/settings/_components/ProviderApiKeys/index.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 
+import { ProviderApiKey } from '@latitude-data/core'
 import useProviderApiKeys from '$/stores/providerApiKeys'
 import useUsers from '$/stores/users'
 import {
@@ -19,11 +20,8 @@ import {
 import NewApiKey from './New'
 
 export default function ProviderApiKeys() {
-  const { data: users } = useUsers()
   const { data: providerApiKeys, destroy } = useProviderApiKeys()
   const [open, setOpen] = useState(false)
-
-  const findUser = (id: string) => users.find((u) => u.id === id)
 
   return (
     <div className='flex flex-col gap-4'>
@@ -35,59 +33,90 @@ export default function ProviderApiKeys() {
         </Button>
       </div>
       <div className='flex flex-col gap-2'>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Name</TableHead>
-              <TableHead>Provider</TableHead>
-              <TableHead>Token</TableHead>
-              <TableHead>Created at</TableHead>
-              <TableHead>Last Used</TableHead>
-              <TableHead>Author</TableHead>
-              <TableHead />
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {providerApiKeys.map((apiKey) => (
-              <TableRow key={apiKey.id}>
-                <TableCell>
-                  <Text.H4>{apiKey.name}</Text.H4>
-                </TableCell>
-                <TableCell>
-                  <Text.H4 color='foregroundMuted'>{apiKey.provider}</Text.H4>
-                </TableCell>
-                <TableCell>
-                  <Text.H4 color='foregroundMuted'>{apiKey.token}</Text.H4>
-                </TableCell>
-                <TableCell>
-                  <Text.H4 color='foregroundMuted'>
-                    {apiKey.createdAt.toDateString()}
-                  </Text.H4>
-                </TableCell>
-                <TableCell>
-                  <Text.H4 color='foregroundMuted'>
-                    {apiKey.lastUsedAt?.toISOString() || 'never'}
-                  </Text.H4>
-                </TableCell>
-                <TableCell>
-                  <Text.H4 color='foregroundMuted'>
-                    {findUser(apiKey.authorId)?.name}
-                  </Text.H4>
-                </TableCell>
-                <TableCell>
-                  <Button
-                    size='small'
-                    variant='linkDestructive'
-                    onClick={() => destroy({ id: apiKey.id })}
-                  >
-                    <Icons.trash />
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        {providerApiKeys.length > 0 && (
+          <ProviderApiKeysTable
+            providerApiKeys={providerApiKeys}
+            destroy={destroy}
+          />
+        )}
+        {providerApiKeys.length === 0 && (
+          <div className='rounded-lg w-full py-12 flex flex-col gap-4 items-center justify-center bg-secondary'>
+            <div className='max-w-[50%]'>
+              <Text.H5 centered display='block' color='foregroundMuted'>
+                There are no API Keys yet. Create one to start working with your
+                prompts.
+              </Text.H5>
+            </div>
+            <Button onClick={() => setOpen(true)}>Create one</Button>
+          </div>
+        )}
       </div>
     </div>
+  )
+}
+
+const ProviderApiKeysTable = ({
+  providerApiKeys,
+  destroy,
+}: {
+  providerApiKeys: ProviderApiKey[]
+  destroy: Function
+}) => {
+  const { data: users } = useUsers()
+  const findUser = (id: string) => users.find((u) => u.id === id)
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Name</TableHead>
+          <TableHead>Provider</TableHead>
+          <TableHead>Token</TableHead>
+          <TableHead>Created at</TableHead>
+          <TableHead>Last Used</TableHead>
+          <TableHead>Author</TableHead>
+          <TableHead />
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {providerApiKeys.map((apiKey) => (
+          <TableRow key={apiKey.id}>
+            <TableCell>
+              <Text.H4>{apiKey.name}</Text.H4>
+            </TableCell>
+            <TableCell>
+              <Text.H4 color='foregroundMuted'>{apiKey.provider}</Text.H4>
+            </TableCell>
+            <TableCell>
+              <Text.H4 color='foregroundMuted'>{apiKey.token}</Text.H4>
+            </TableCell>
+            <TableCell>
+              <Text.H4 color='foregroundMuted'>
+                {apiKey.createdAt.toDateString()}
+              </Text.H4>
+            </TableCell>
+            <TableCell>
+              <Text.H4 color='foregroundMuted'>
+                {apiKey.lastUsedAt?.toISOString() || 'never'}
+              </Text.H4>
+            </TableCell>
+            <TableCell>
+              <Text.H4 color='foregroundMuted'>
+                {findUser(apiKey.authorId)?.name}
+              </Text.H4>
+            </TableCell>
+            <TableCell>
+              <Button
+                size='small'
+                variant='linkDestructive'
+                onClick={() => destroy({ id: apiKey.id })}
+              >
+                <Icons.trash />
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
