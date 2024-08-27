4d467227da1480847cce274a5f263af9dc7165d5
Gerard <clos.gerard@gmail.com>
2024-08-27 09:54:10 +0200

    feat: deploy from CI (#125)

--- apps/infra/src/index.ts
+++ apps/infra/src/index.ts
@@ -3,13 +3,13 @@ import * as pulumi from '@pulumi/pulumi'
 const stack = pulumi.getStack()
 
 let result
-if (stack === 'core') {
+if (stack.startsWith('core')) {
   result = await import('./core')
-} else if (stack === 'web') {
+} else if (stack.startsWith('web')) {
   result = await import('./deployments/web')
-} else if (stack === 'gateway') {
+} else if (stack.startsWith('gateway')) {
   result = await import('./deployments/gateway')
-} else if (stack === 'workers') {
+} else if (stack.startsWith('workers')) {
   result = await import('./deployments/workers')
 } else {
   console.error(`Unknown stack: ${stack}`)
