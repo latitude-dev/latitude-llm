e749d6e586b9176cbc91b2bd4e494c0eeb6e2360
Gerard Clos <clos.gerard@gmail.com>
2024-09-09 12:39:05 +0200

    minor: added go home button in error page

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@latitude-data/web-ui'
 import { ErrorComponent } from '@latitude-data/web-ui/browser'
 import { NAV_LINKS } from '$/app/(private)/_lib/constants'
 import { AppLayout } from '$/components/layouts'
 import { getSafeCurrentUser } from '$/services/auth/getCurrentUser'
+import { ROUTES } from '$/services/routes'
+import Link from 'next/link'
 
 export default async function GlobalNoFound() {
   const session = await getSafeCurrentUser()
@@ -12,8 +15,13 @@ export default async function GlobalNoFound() {
       navigationLinks={NAV_LINKS}
     >
       <ErrorComponent
-        message="We couldn't find what you are looking for. Please make sure that the page exists and try again."
         type='gray'
+        message="We couldn't find what you are looking for. Please make sure that the page exists and try again."
+        submit={
+          <Link href={ROUTES.root}>
+            <Button>Go to Homepage</Button>
+          </Link>
+        }
       />
     </AppLayout>
   )
--- packages/web-ui/src/ds/molecules/ErrorComponent/index.tsx
+++ packages/web-ui/src/ds/molecules/ErrorComponent/index.tsx
@@ -1,17 +1,21 @@
+import { ReactNode } from 'react'
+
 import { cn } from '../../../lib/utils'
 import { Icon, Text } from '../../atoms'
 
 export function ErrorComponent({
   message,
   type,
+  submit,
 }: {
   message: string
   type: 'gray' | 'red'
+  submit?: ReactNode
 }) {
   return (
-    <div className='w-full h-full flex items-center justify-center'>
+    <div className='w-full h-full flex flex-col items-center justify-center gap-4 max-w-80 m-auto'>
       <div
-        className={cn('flex flex-col items-center gap-y-8 max-w-80', {
+        className={cn('flex flex-col items-center justify-center gap-y-8', {
           'text-muted-foreground': type === 'gray',
           'text-destructive': type === 'red',
         })}
@@ -21,6 +25,7 @@ export function ErrorComponent({
           {message}
         </Text.H5>
       </div>
+      {submit}
     </div>
   )
 }
