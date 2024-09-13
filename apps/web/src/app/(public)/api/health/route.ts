6daae4ca4b8f311d74e3bffe7d8ac8f31c96e4c8
Gerard Clos <clos.gerard@gmail.com>
2024-09-13 14:54:52 +0200

    quickfix: andres seteas bien las env vars o k ase

--- apps/web/src/app/(public)/api/health/route.ts
+++ apps/web/src/app/(public)/api/health/route.ts
@@ -1,3 +1,8 @@
+import env from '$/env'
+
 export async function GET() {
+  // NOTE: This is so the health check fails if the env is not correctly loaded
+  console.log(env)
+
   return Response.json({ status: 'ok' })
 }
