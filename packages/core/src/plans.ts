098fde8400b0ac0da0b69be2187d08730e5db733
Gerard <clos.gerard@gmail.com>
2024-10-08 15:22:01 +0200

    feature: subscriptions data model (#367)

---
+++ packages/core/src/plans.ts
@@ -0,0 +1,29 @@
+export enum SubscriptionPlan {
+  EnterpriseV1 = 'enterprise_v1',
+  HobbyV1 = 'hobby_v1',
+  HobbyV2 = 'hobby_v2',
+  TeamV1 = 'team_v1',
+}
+
+export const SubscriptionPlans = {
+  [SubscriptionPlan.HobbyV2]: {
+    name: 'Hobby',
+    credits: 20_000,
+    users: 1,
+  },
+  [SubscriptionPlan.HobbyV1]: {
+    name: 'Hobby',
+    credits: 50_000,
+    users: undefined,
+  },
+  [SubscriptionPlan.TeamV1]: {
+    name: 'Team',
+    credits: 100_000,
+    users: undefined,
+  },
+  [SubscriptionPlan.EnterpriseV1]: {
+    name: 'Enterprise',
+    credits: undefined,
+    users: undefined,
+  },
+}
---
+++ packages/core/src/schema/models/subscriptions.ts
@@ -0,0 +1,28 @@
+import { bigint, bigserial, index } from 'drizzle-orm/pg-core'
+
+import { SubscriptionPlan } from '../../plans'
+import { latitudeSchema } from '../db-schema'
+import { timestamps } from '../schemaHelpers'
+
+export const plansEnum = latitudeSchema.enum('subscription_plans', [
+  SubscriptionPlan.HobbyV1,
+  SubscriptionPlan.HobbyV2,
+  SubscriptionPlan.TeamV1,
+  SubscriptionPlan.EnterpriseV1,
+])
+
+export const subscriptions = latitudeSchema.table(
+  'subscriptions',
+  {
+    id: bigserial('id', { mode: 'number' }).notNull().primaryKey(),
+    workspaceId: bigint('workspace_id', { mode: 'number' }).notNull(),
+    plan: plansEnum('plan').notNull(),
+    ...timestamps(),
+  },
+  (table) => {
+    return {
+      workspaceIdIndex: index().on(table.workspaceId),
+      planIndex: index().on(table.plan),
+    }
+  },
+)
