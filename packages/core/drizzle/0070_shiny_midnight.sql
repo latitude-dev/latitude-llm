23793250597d230fcdc95744236c8a9b8f636767
Gerard Clos <clos.gerard@gmail.com>
2024-10-09 10:21:54 +0200

    feat: migrate subscription created_ats to workspaces' created_at
    So to keep the current credit count consistent

---
+++ packages/core/drizzle/0070_shiny_midnight.sql
@@ -0,0 +1,10 @@
+-- Custom SQL migration file, put you code below! --
+BEGIN;
+
+-- Update the created_at column in the subscriptions table
+UPDATE subscriptions
+SET created_at = workspaces.created_at
+FROM workspaces
+WHERE subscriptions.id = workspaces.current_subscription_id;
+
+COMMIT;
