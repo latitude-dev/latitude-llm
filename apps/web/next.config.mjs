009dc0ad878e1feead8a8a417a9430d549e9b45f
Gerard Clos <clos.gerard@gmail.com>
2024-10-15 17:51:28 +0200

    feat: do not hide source maps from client bundles (helps debugging)

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -60,7 +60,7 @@ if (process.env.SENTRY_ORG && process.env.SENTRY_PROJECT) {
     tunnelRoute: '/monitoring',
 
     // Hides source maps from generated client bundles
-    hideSourceMaps: true,
+    hideSourceMaps: false,
 
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
