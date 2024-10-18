cfdbfbb9c5a88f96f7765afbef3d07a6671a5387
andresgutgon <andresgutgon@gmail.com>
2024-10-18 20:26:18 +0200

    Change design for batch evaluation run progress bar. We unify in a unique line completed and errored evaluation counters

--- packages/web-ui/src/ds/atoms/Alert/index.tsx
+++ packages/web-ui/src/ds/atoms/Alert/index.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react'
 
-import { cn } from '../../../lib/utils'
-import { Icon, IconName } from '../Icons'
+import { Icon } from '../Icons'
 import {
   AlertDescription,
   AlertProps,
@@ -32,41 +31,6 @@ export function Alert({
           <AlertDescription>{description}</AlertDescription>
         </div>
         {cta}
-      </div>
-    </AlertRoot>
-  )
-}
-
-type ProgressState = 'completed' | 'error' | 'running'
-const ICON_NAME: Record<ProgressState, IconName> = {
-  completed: 'check',
-  error: 'alert',
-  running: 'refresh',
-}
-const VARIANT_PROGRESS: Record<ProgressState, AlertProps['variant']> = {
-  completed: 'success',
-  error: 'destructive',
-  running: 'default',
-}
-
-type ProgressIndicatorProps = {
-  children: ReactNode
-  state?: ProgressState
-}
-export function ProgressIndicator({
-  children,
-  state = 'completed',
-}: ProgressIndicatorProps) {
-  const running = state === 'running'
-  const variant = VARIANT_PROGRESS[state]
-  return (
-    <AlertRoot variant={variant}>
-      <div className='flex flex-row items-center gap-x-2'>
-        <Icon
-          name={ICON_NAME[state]}
-          className={cn({ 'animate-spin': running })}
-        />
-        <div>{children}</div>
       </div>
     </AlertRoot>
   )
