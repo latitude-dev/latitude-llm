81d2da487b5e0f1e4e164f31a94a8d52cc4fc62a
Andrés <andresgutgon@gmail.com>
2024-10-23 15:53:20 +0200

    Limit the amount of vertical space commit selector takes. But take all (#498)
    it can

--- packages/web-ui/src/ds/atoms/Select/Primitives/index.tsx
+++ packages/web-ui/src/ds/atoms/Select/Primitives/index.tsx
@@ -148,20 +148,31 @@ const SelectContent = forwardRef<
   ElementRef<typeof SelectPrimitive.Content>,
   ComponentPropsWithoutRef<typeof SelectPrimitive.Content> & {
     autoScroll?: boolean
+    maxHeightAuto?: boolean
   }
 >(
   (
-    { className, children, autoScroll = true, position = 'popper', ...props },
+    {
+      className,
+      children,
+      autoScroll = true,
+      maxHeightAuto = false,
+      position = 'popper',
+      ...props
+    },
     ref,
   ) => (
     <SelectPrimitive.Portal>
       <SelectPrimitive.Content
         ref={ref}
         className={cn(
-          'relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+          'relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
           position === 'popper' &&
             'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
           className,
+          {
+            'max-h-96': !maxHeightAuto,
+          },
         )}
         position={position}
         {...props}
