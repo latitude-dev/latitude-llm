8c34e369a21f608d6f672daa69e68447b34914fe
andresgutgon <andresgutgon@gmail.com>
2024-10-23 11:47:43 +0200

    Enable system mode 🤡

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute='class'
           defaultTheme='light'
-          enableSystem={false}
+          enableSystem
           disableTransitionOnChange
         >
           <TooltipProvider>{children}</TooltipProvider>
