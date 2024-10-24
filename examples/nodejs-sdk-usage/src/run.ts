cd3fc117cd289eb22936c37038679f10b7257021
andresgutgon <andresgutgon@gmail.com>
2024-10-24 17:24:11 +0200

    feat: wip

--- examples/nodejs-sdk-usage/src/run.ts
+++ examples/nodejs-sdk-usage/src/run.ts
@@ -5,13 +5,17 @@ import { Latitude } from '@latitude-data/sdk'
 
 async function makeSdkRequest() {
   const apiKey = process.env.LATITUDE_API_KEY
+  const projectId = process.env.LATITUDE_PROJECT_ID
   if (!apiKey) {
     throw new Error('Latitude API key is required')
   }
 
   console.log("API_KEY", apiKey)
 
-  /* const sdk = new Latitude(apiKey) */
+  const sdk = new Latitude(apiKey)
+  const response = await sdk.run('run-document', {
+
+  })
 }
 
 makeSdkRequest()
