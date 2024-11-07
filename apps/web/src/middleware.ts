cdb24c8e1f660def2cecb7414f83a2bf341ecbc8
andresgutgon <andresgutgon@gmail.com>
2024-11-07 17:32:18 +0100

    Skip favicon.svg from nextjs middleware. Puto NextJS So we had favicon.ico but not favicon.svg and this requests was all the time redirecting to /login

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -24,6 +24,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|favicon.svg|sitemap.xml|robots.txt).*)',
   ],
 }
