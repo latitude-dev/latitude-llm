e556a8ffa1ef9d44e205cfe57c01393a6d778076
andresgutgon <andresgutgon@gmail.com>
2024-11-15 09:18:28 +0100

    Redirect to log correct page in paginated logs list We were doing a shitty version of this in the client with a useEffect and I realize that we have a backend. So I just moved to it

--- apps/web/src/app/(private)/projects/[projectId]/versions/[commitUuid]/documents/[documentUuid]/logs/page.tsx
+++ apps/web/src/app/(private)/projects/[projectId]/versions/[commitUuid]/documents/[documentUuid]/logs/page.tsx
@@ -1,12 +1,34 @@
+import { Workspace } from '@latitude-data/core/browser'
 import { QueryParams } from '@latitude-data/core/lib/pagination/buildPaginatedUrl'
 import { computeDocumentLogsWithMetadataQuery } from '@latitude-data/core/services/documentLogs/computeDocumentLogsWithMetadata'
+import { fetchDocumentLogWithPosition } from '@latitude-data/core/services/documentLogs/fetchDocumentLogWithPosition'
 import { Button, TableWithHeader } from '@latitude-data/web-ui'
 import { findCommitCached } from '$/app/(private)/_data-access'
 import { getCurrentUser } from '$/services/auth/getCurrentUser'
 import { ROUTES } from '$/services/routes'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 import { DocumentLogs } from './_components/DocumentLogs'
+
+async function fetchDocumentLogPage({
+  workspace,
+  documentLogUuid,
+}: {
+  workspace: Workspace
+  documentLogUuid: string | undefined
+}) {
+  if (!documentLogUuid) return undefined
+
+  const result = await fetchDocumentLogWithPosition({
+    workspace,
+    documentLogUuid,
+  })
+
+  if (result.error) return undefined
+
+  return result.value.page.toString()
+}
 
 export default async function DocumentPage({
   params,
@@ -18,27 +40,42 @@ export default async function DocumentPage({
   const { workspace } = await getCurrentUser()
   const projectId = Number(params.projectId)
   const commitUuid = params.commitUuid
+  const documentUuid = params.documentUuid
   const commit = await findCommitCached({ projectId, uuid: commitUuid })
+  const documentLogUuid = searchParams.logUuid?.toString()
+  const page = searchParams.page?.toString?.()
+  const currentLogPage = await fetchDocumentLogPage({
+    workspace,
+    documentLogUuid,
+  })
+
+  if (currentLogPage && currentLogPage !== page) {
+    const route = ROUTES.projects
+      .detail({ id: projectId })
+      .commits.detail({ uuid: commit.uuid })
+      .documents.detail({ uuid: documentUuid }).logs.root
+    return redirect(
+      `${route}?page=${currentLogPage}&logUuid=${documentLogUuid}`,
+    )
+  }
+
+  // Paginated logs
   const rows = await computeDocumentLogsWithMetadataQuery({
     workspaceId: workspace.id,
-    documentUuid: params.documentUuid,
+    documentUuid,
     draft: commit,
-    page: searchParams.page as string | undefined,
+    page,
     pageSize: searchParams.pageSize as string | undefined,
   })
-  const documentLogUuid = searchParams.logUuid?.toString()
+
+  // Current log in that page
   const selectedLog = rows.find((r) => r.uuid === documentLogUuid)
+
   return (
     <div className='flex flex-grow min-h-0 flex-col w-full p-6 gap-2 min-w-0'>
       <TableWithHeader
         title='Logs'
-        table={
-          <DocumentLogs
-            documentLogs={rows}
-            documentLogUuid={documentLogUuid}
-            selectedLog={selectedLog}
-          />
-        }
+        table={<DocumentLogs documentLogs={rows} selectedLog={selectedLog} />}
         actions={
           <Link
             href={
