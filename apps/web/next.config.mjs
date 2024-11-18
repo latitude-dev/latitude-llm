218505dae79d03319b136a579b0cab02b5d706d1
Gerard Clos <clos.gerard@gmail.com>
2024-11-18 06:38:11 +0100

    feat(sentry): do not route frontend errors through proxy
    Frontend errors are not arriving to Sentry. Testing whether it's the
    automatic proxying that's causing the issue.

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -63,7 +63,7 @@ if (process.env.SENTRY_ORG && process.env.SENTRY_PROJECT) {
     // This can increase your server load as well as your hosting bill.
     // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
     // side errors will fail.
-    tunnelRoute: '/monitoring',
+    // tunnelRoute: '/monitoring',
 
     // Hides source maps from generated client bundles
     hideSourceMaps: false,
