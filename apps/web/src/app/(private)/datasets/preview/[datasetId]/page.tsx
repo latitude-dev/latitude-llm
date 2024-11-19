8ff0d76736c7a86393f198c9b1acd5a38b00661e
Alex Rodríguez <alex@latitude.so>
2024-11-19 12:40:41 +0100

    Fix feat/metatags (#641)
    * Revert "Revert "Add meta tags (#632)""
    
    This reverts commit ecd9fd144f8f429a5986f10f7893d4449e2d34ad.
    
    * Add LATITUDE_URL to build
    
    * Add LATITUDE_URL to more files
    
    * Fix lint

--- apps/web/src/app/(private)/datasets/preview/[datasetId]/page.tsx
+++ apps/web/src/app/(private)/datasets/preview/[datasetId]/page.tsx
@@ -1,6 +1,33 @@
+import { NotFoundError } from '@latitude-data/core/lib/errors'
+import buildMetatags from '$/app/_lib/buildMetatags'
 import { getDatasetCached } from '$/app/(private)/_data-access'
+import type { ResolvingMetadata } from 'next'
+import { notFound } from 'next/navigation'
 
 import PreviewDatasetModal from './_components/PreviewDatasetModal'
+
+export async function generateMetadata(
+  {
+    params,
+  }: {
+    params: Promise<{ datasetId: string }>
+  },
+  parent: ResolvingMetadata,
+) {
+  const { datasetId } = await params
+
+  try {
+    const dataset = await getDatasetCached(datasetId)
+
+    return buildMetatags({
+      title: `${dataset.name} (preview)`,
+      parent: await parent,
+    })
+  } catch (error) {
+    if (error instanceof NotFoundError) return notFound()
+    throw error
+  }
+}
 
 export default async function DatasetPreviewPage({
   params,
