926f89d6b2cfe5c538e98ee3c87db05613d526ec
andresgutgon <andresgutgon@gmail.com>
2024-11-28 17:06:15 +0100

    No, thank you very much

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: ReactNode
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang='en' translate='no' suppressHydrationWarning>
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
