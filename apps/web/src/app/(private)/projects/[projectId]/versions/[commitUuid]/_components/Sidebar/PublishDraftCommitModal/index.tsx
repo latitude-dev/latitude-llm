cac7cf8ce16bb190accd722676472521365c5362
Gerard Clos <clos.gerard@gmail.com>
2024-12-02 09:48:03 +0100

    fixes LATITUDE-LLM-APP-C1

--- apps/web/src/app/(private)/projects/[projectId]/versions/[commitUuid]/_components/Sidebar/PublishDraftCommitModal/index.tsx
+++ apps/web/src/app/(private)/projects/[projectId]/versions/[commitUuid]/_components/Sidebar/PublishDraftCommitModal/index.tsx
@@ -299,6 +299,7 @@ export default function PublishDraftCommitModal({
         })
         return
       }
+      if (!data) return // should not happen but it does
 
       setGroups(
         data.reduce(
