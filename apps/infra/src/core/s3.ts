b372d67c51ee27726280c36d58f486a61e6eabcf
Alex <me@arn.sh>
2024-12-13 15:37:00 +0100

    Make bucket public

--- apps/infra/src/core/s3.ts
+++ apps/infra/src/core/s3.ts
@@ -22,7 +22,7 @@ export const bucketName = bucket.bucket
 export const publicBucket = new aws.s3.BucketV2(
   'publicLatitudeBucketResource',
   {
-    acl: 'private', // Only allowing access through signed urls
+    acl: 'private', // Keep ACL private to disallow all actions by default
     bucket: 'latitude-llm-public-bucket-production',
     tags: {
       Name: 'Latitude LLM public bucket',
@@ -33,3 +33,25 @@ export const publicBucket = new aws.s3.BucketV2(
 )
 
 export const publicBucketName = publicBucket.bucket
+
+new aws.s3.BucketPolicy(
+  'publicLatitudeBucketPolicy',
+  {
+    bucket: publicBucket.id,
+    policy: publicBucket.arn.apply((bucketArn) =>
+      JSON.stringify({
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Sid: 'PublicReadGetObject',
+            Effect: 'Allow',
+            Principal: '*',
+            Action: 's3:GetObject', // Note: all actions are disallowed by default
+            Resource: `${bucketArn}/*`,
+          },
+        ],
+      }),
+    ),
+  },
+  { provider: regionProvider },
+)
