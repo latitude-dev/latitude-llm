2cfe5ee73419436220e34243f00b1aed8b0465ea
Gerard Clos <clos.gerard@gmail.com>
2025-01-16 10:35:33 +0100

    chore: disable tracing all latitude generations

--- apps/gateway/src/instrumentation.ts
+++ apps/gateway/src/instrumentation.ts
@@ -1,11 +1,1 @@
-import { env } from '@latitude-data/env'
-import { VercelBatchSpanProcessor } from '@latitude-data/telemetry'
-import { Latitude } from '@latitude-data/sdk'
-
-export const latitude = env.COPILOT_WORKSPACE_API_KEY
-  ? new Latitude(env.COPILOT_WORKSPACE_API_KEY, {
-      telemetry: {
-        processors: [VercelBatchSpanProcessor],
-      },
-    })
-  : undefined
+export const latitude = undefined
