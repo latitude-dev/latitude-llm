6c538472a09a538d515e2e339ff321b13192c5fd
Alex <me@arn.sh>
2025-02-11 12:08:16 +0100

    Add eval editor link to eval results in log table

--- apps/web/src/app/(private)/projects/[projectId]/versions/[commitUuid]/documents/[documentUuid]/logs/_components/DocumentLogs/DocumentLogInfo/Evaluations.tsx
+++ apps/web/src/app/(private)/projects/[projectId]/versions/[commitUuid]/documents/[documentUuid]/logs/_components/DocumentLogs/DocumentLogInfo/Evaluations.tsx
@@ -1,26 +1,23 @@
 import {
+  evaluationMetadataTypes,
+  evaluationResultTypes,
+} from '$/app/(private)/evaluations/_components/ActiveEvaluations/Table'
+import { useCurrentDocument } from '$/app/providers/DocumentProvider'
+import { EvaluationRoutes, ROUTES } from '$/services/routes'
+import {
   EvaluationMetadataType,
   EvaluationResultableType,
 } from '@latitude-data/core/browser'
 import { ResultWithEvaluation } from '@latitude-data/core/repositories'
 import {
   Button,
-  Icon,
   Text,
   useCurrentCommit,
   useCurrentProject,
 } from '@latitude-data/web-ui'
-import {
-  evaluationMetadataTypes,
-  evaluationResultTypes,
-} from '$/app/(private)/evaluations/_components/ActiveEvaluations/Table'
-import { useCurrentDocument } from '$/app/providers/DocumentProvider'
-import { ROUTES } from '$/services/routes'
 import Link from 'next/link'
-
 import { MetadataItem } from '../../../../_components/MetadataItem'
 import { ResultCellContent } from '../../../../evaluations/[evaluationId]/_components/EvaluationResults/EvaluationResultsTable'
-
 function EvaluationResultItem({ result, evaluation }: ResultWithEvaluation) {
   if (result.resultableType === EvaluationResultableType.Text) {
     return (
@@ -38,17 +35,33 @@ function evaluationResultLink({ result, evaluation }: ResultWithEvaluation) {
   const { commit } = useCurrentCommit()
   const { document } = useCurrentDocument()
 
-  const route = ROUTES.projects
-    .detail({ id: project.id })
-    .commits.detail({ uuid: commit.uuid })
-    .documents.detail({ uuid: document.documentUuid })
-    .evaluations.detail(evaluation.id).root
-
+  const query = new URLSearchParams()
   if (evaluation.metadataType === EvaluationMetadataType.Manual) {
-    return `${route}?documentLogId=${result.documentLogId}`
+    query.set('documentLogId', result.documentLogId.toString())
+  } else {
+    query.set('resultUuid', result.uuid)
   }
 
-  return `${route}?resultUuid=${result.uuid}`
+  return (
+    ROUTES.projects
+      .detail({ id: project.id })
+      .commits.detail({ uuid: commit.uuid })
+      .documents.detail({ uuid: document.documentUuid })
+      .evaluations.detail(evaluation.id).root + `?${query.toString()}`
+  )
+}
+
+function evaluationEditorLink({ result, evaluation }: ResultWithEvaluation) {
+  const query = new URLSearchParams()
+  if (result.evaluatedProviderLogId) {
+    query.set('providerLogId', result.evaluatedProviderLogId.toString())
+  }
+
+  return (
+    ROUTES.evaluations.detail({ uuid: evaluation.uuid })[
+      EvaluationRoutes.editor
+    ].root + `?${query.toString()}`
+  )
 }
 
 export function DocumentLogEvaluations({
@@ -68,9 +81,27 @@ export function DocumentLogEvaluations({
     <ul className='flex flex-col gap-4 divide-y divide-border'>
       {evaluationResults.map(({ result, evaluation }) => (
         <li key={result.uuid} className='flex flex-col gap-2 pt-4 first:pt-0'>
-          <Text.H4M noWrap ellipsis>
-            {evaluation.name}
-          </Text.H4M>
+          <span className='flex justify-between items-center gap-2 w-full'>
+            <Text.H4M noWrap ellipsis>
+              {evaluation.name}
+            </Text.H4M>
+            <Link
+              href={evaluationEditorLink({ result, evaluation })}
+              target='_blank'
+            >
+              <Button
+                variant='link'
+                iconProps={{
+                  name: 'externalLink',
+                  widthClass: 'w-4',
+                  heightClass: 'h-4',
+                  placement: 'right',
+                }}
+              >
+                Edit
+              </Button>
+            </Link>
+          </span>
           <div className='flex flex-col gap-2.5'>
             <MetadataItem
               stacked
@@ -86,14 +117,13 @@ export function DocumentLogEvaluations({
               value={result.reason || '-'}
             />
           </div>
-          <div className={'w-full flex justify-center'}>
+          <div className='w-full flex justify-start'>
             <Link
               href={evaluationResultLink({ result, evaluation })}
               target='_blank'
             >
-              <Button variant='link'>
-                Check evaluation result
-                <Icon name='externalLink' widthClass='w-4' heightClass='h-4' />
+              <Button variant='outline' fancy>
+                View log
               </Button>
             </Link>
           </div>
