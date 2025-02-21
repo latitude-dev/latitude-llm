142e3181ee7fdcc0f90aaa251cf6548e02f54d80
Gerard Clos <clos.gerard@gmail.com>
2025-02-21 10:49:48 +0100

    feat: stdout logging in nextjs production mode

--- apps/web/src/instrumentation.ts
+++ apps/web/src/instrumentation.ts
@@ -2,10 +2,18 @@ import * as Sentry from '@sentry/nextjs'
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    // Sentry
     await import('../sentry.server.config')
+
+    if (process.env.NODE_ENV === 'production') {
+      // Stdout logging
+      await import('pino')
+      await import('next-logger')
+    }
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
+    // Sentry
     await import('../sentry.edge.config')
   }
 }
