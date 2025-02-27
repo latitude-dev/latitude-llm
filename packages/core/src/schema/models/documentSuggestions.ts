635dbe53a0e868addf65110ce101a99e14a1e997
Alex <me@arn.sh>
2025-02-27 09:38:42 +0100

    Add new columns to allow for patch suggestion apply

--- packages/core/src/schema/models/documentSuggestions.ts
+++ packages/core/src/schema/models/documentSuggestions.ts
@@ -11,21 +11,29 @@ import { latitudeSchema } from '../db-schema'
 import { timestamps } from '../schemaHelpers'
 import { documentVersions } from './documentVersions'
 import { evaluations } from './evaluations'
+import { workspaces } from './workspaces'
 
 export const documentSuggestions = latitudeSchema.table(
   'document_suggestions',
   {
     id: bigserial('id', { mode: 'number' }).notNull().primaryKey(),
+    workspaceId: bigint('workspace_id', { mode: 'number' }) // TODO: Add .notNull() when migration is done
+      .references(() => workspaces.id, { onDelete: 'cascade' }),
     commitId: bigint('commit_id', { mode: 'number' }).notNull(),
     documentUuid: uuid('document_uuid').notNull(),
     evaluationId: bigint('evaluation_id', { mode: 'number' })
       .notNull()
       .references(() => evaluations.id, { onDelete: 'cascade' }),
+    oldPrompt: text('old_prompt'), // TODO: Add .notNull() when migration is done
+    newPrompt: text('new_prompt'), // TODO: Add .notNull() when migration is done
     prompt: text('prompt').notNull(),
     summary: text('summary').notNull(),
     ...timestamps(),
   },
   (table) => ({
+    workspaceIdIdx: index('document_suggestions_workspace_id_idx').on(
+      table.workspaceId,
+    ),
     documentVersionsFk: foreignKey({
       columns: [table.commitId, table.documentUuid],
       foreignColumns: [
