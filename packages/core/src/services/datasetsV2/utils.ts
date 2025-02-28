e5aaca0710cfc40c742876b0a111bd1643547a43
andresgutgon <andresgutgon@gmail.com>
2025-02-28 19:13:45 +0100

    feat: wip

--- packages/core/src/services/datasetsV2/utils.ts
+++ packages/core/src/services/datasetsV2/utils.ts
@@ -1,5 +1,5 @@
 export type HashAlgorithmFn = (len: number) => string
-export const buildColumn = (hashAlgorithm: HashAlgorithmFn) => (column: string) => {
+export const buildColumn = <C extends string = string>(hashAlgorithm: HashAlgorithmFn) => (column: C) => {
   return {
     identifier: hashAlgorithm(7),
     name: column,
