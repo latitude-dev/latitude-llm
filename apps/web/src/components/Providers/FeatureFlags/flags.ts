15ccfdea759beb67387c96059422369feaea7590
andresgutgon <andresgutgon@gmail.com>
2025-03-19 18:23:51 +0100

    Stop using datasets v2 for latitude

--- apps/web/src/components/Providers/FeatureFlags/flags.ts
+++ apps/web/src/components/Providers/FeatureFlags/flags.ts
@@ -12,7 +12,7 @@ export const FEATURE_FLAGS_CONDITIONS: Record<
   FeatureFlag,
   FeatureFlagCondition
 > = {
-  datasetsV2: { workspaceIds: [1] },
+  datasetsV2: { workspaceIds: [] },
   datasetsV1ModificationBlocked: { workspaceIds: [] },
 }
 
