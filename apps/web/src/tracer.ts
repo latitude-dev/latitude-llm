aca3f58824958cb2318633e6f3ded7c8a8047d8d
Gerard Clos <clos.gerard@gmail.com>
2025-03-22 16:30:09 +0100

    chore: enable datadog profiling

--- apps/web/src/tracer.ts
+++ apps/web/src/tracer.ts
@@ -1,8 +1,13 @@
 import tracer from 'dd-trace'
+import { env } from '@latitude-data/env'
 
 tracer.init({
   logInjection: true,
   runtimeMetrics: true,
+  profiling: env.NODE_ENV === 'production',
+  env: env.NODE_ENV === 'production' ? 'prod' : 'dev',
+  service: 'latitude-data/web',
+  version: '1.0.0',
 })
 
 export default tracer
