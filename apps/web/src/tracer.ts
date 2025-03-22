794c0b678afde61f37d5c2fe68f8d57aaea6453d
Gerard Clos <clos.gerard@gmail.com>
2025-03-22 19:31:06 +0100

    chore: tweak profiling settings

--- apps/web/src/tracer.ts
+++ apps/web/src/tracer.ts
@@ -4,10 +4,7 @@ import { env } from '@latitude-data/env'
 tracer.init({
   logInjection: true,
   runtimeMetrics: true,
-  profiling: env.NODE_ENV === 'production',
-  env: env.NODE_ENV === 'production' ? 'prod' : 'dev',
-  service: 'latitude-data/web',
-  version: '1.0.0',
+  profiling: true,
 })
 
 export default tracer
