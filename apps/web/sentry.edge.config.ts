036a9111dddb98a0605c7004e2d027c09c9b8791
Gerard <clos.gerard@gmail.com>
2025-03-24 14:30:20 +0100

    feat: split into multiple sentry projects (#1017)
    We are splitting each project into its own sentry project.

--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -6,8 +6,8 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  enabled: !!process.env.SENTRY_DSN,
+  dsn: process.env.SENTRY_WEB_DSN,
+  enabled: !!process.env.SENTRY_WEB_DSN,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
--- apps/web/src/helpers/captureException.ts
+++ apps/web/src/helpers/captureException.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nextjs'
 import { env } from '@latitude-data/env'
 
 export const captureException = (error: Error) => {
-  if (env.SENTRY_DSN) {
+  if (env.SENTRY_WEB_DSN) {
     Sentry.captureException(error)
   } else {
     console.error(error)
@@ -10,7 +10,7 @@ export const captureException = (error: Error) => {
 }
 
 export const captureMessage = (message: string) => {
-  if (env.SENTRY_DSN) {
+  if (env.SENTRY_WEB_DSN) {
     Sentry.captureMessage(message)
   } else {
     console.log(message)
