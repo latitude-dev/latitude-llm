ea3fc1b11843ffc1995cdf8f6e0e5a430f7d19bb
Gerard <clos.gerard@gmail.com>
2025-04-02 10:06:14 +0200

    feat: fix tsup-based apps docker images (#1072)
    These images were broken as a result of the changes in the previous
    commits. This commit fixes them.

--- apps/websockets/tsup.config.ts
+++ apps/websockets/tsup.config.ts
@@ -1,16 +1,4 @@
-import { readFileSync } from 'fs'
-
 import { defineConfig } from 'tsup'
-
-const getDependencies = (path: string) => {
-  const deps = JSON.parse(readFileSync(path, 'utf-8')).dependencies
-  if (!deps) return []
-
-  return Object.keys(deps)
-}
-
-const rootDependencies = getDependencies('../../package.json')
-const dependencies = getDependencies('./package.json')
 
 export default defineConfig({
   entry: ['src/server.ts'],
@@ -26,7 +14,7 @@ export default defineConfig({
     // So we just tell it to ignore it using 'empty' loader
     '.html': 'empty',
   },
-  external: [...rootDependencies, ...dependencies],
+  skipNodeModulesBundle: true,
   noExternal: [
     '@latitude-data/env',
     '@latitude-data/core',
