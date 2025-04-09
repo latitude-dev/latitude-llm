71e90768ca8343aa62e9902b7901ae050833a989
Alex <me@arn.sh>
2025-04-09 09:45:46 +0200

    Bump python promptl version

--- packages/sdks/python/tests/prompts/render_test.py
+++ packages/sdks/python/tests/prompts/render_test.py
@@ -1,6 +1,16 @@
 from unittest import mock
 
-from promptl_ai import Adapter, Error, ErrorPosition, PromptlError, SystemMessage, TextContent, UserMessage, openai
+from promptl_ai import (
+    Adapter,
+    Error,
+    ErrorPosition,
+    PromptlError,
+    SystemMessage,
+    TextContent,
+    UserMessage,
+    anthropic,
+    openai,
+)
 
 from latitude_sdk import RenderPromptOptions, RenderPromptResult
 from tests.utils import TestCase, fixtures
@@ -33,7 +43,7 @@ class TestRenderPrompt(TestCase):
             ),
         )
 
-    async def test_success_with_adapter(self):
+    async def test_success_with_adapter_singlepart(self):
         prompt = fixtures.PROMPT.content
         options = RenderPromptOptions(
             parameters={"question": "What is PromptL"},
@@ -55,6 +65,60 @@ class TestRenderPrompt(TestCase):
                     "temperature": 0.5,
                     "max_tokens": 1024,
                     "top_p": 0.9,
+                },
+            ),
+        )
+
+    async def test_success_with_adapter_multipart(self):
+        parts = fixtures.PROMPT.content.split("</step>")
+        prompt = "</step>".join(
+            [
+                parts[0]
+                + """
+<user>
+    This is text.
+    <content-image>This is an image.</content-image>
+    <content-file mime="application/pdf">This is a PDF.</content-file>
+</user>
+""",
+                *parts[1:],
+            ]
+        )
+        options = RenderPromptOptions(
+            parameters={"question": "What is PromptL"},
+            adapter=Adapter.Anthropic,
+        )
+
+        result = await self.sdk.prompts.render(prompt, options)
+
+        self.assertEqual(
+            result,
+            RenderPromptResult(
+                messages=[
+                    anthropic.UserMessage(content=[anthropic.TextContent(text="What is PromptL")]),
+                    anthropic.UserMessage(
+                        content=[
+                            anthropic.TextContent(text="This is text."),
+                            anthropic.ImageContent(
+                                source=anthropic.ContentSource(
+                                    type="base64", media_type="image/png", data="This is an image."
+                                )
+                            ),
+                            anthropic.DocumentContent(
+                                source=anthropic.ContentSource(
+                                    type="base64", media_type="application/pdf", data="This is a PDF."
+                                )
+                            ),
+                        ]
+                    ),
+                ],
+                config={
+                    "provider": "OpenAI",
+                    "model": "gpt-4o-mini",
+                    "temperature": 0.5,
+                    "max_tokens": 1024,
+                    "top_p": 0.9,
+                    "system": [{"type": "text", "text": "You are a helpful assistant."}],
                 },
             ),
         )
