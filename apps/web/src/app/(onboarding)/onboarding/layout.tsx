3610881ffc5f2a2ae917fec6e9c3a1ee248bb639
Gerard Clos <clos.gerard@gmail.com>
2025-04-23 10:27:46 +0200

    chore: add posthog provider to onboarding page

--- apps/web/src/app/(onboarding)/onboarding/layout.tsx
+++ apps/web/src/app/(onboarding)/onboarding/layout.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { Icon } from '@latitude-data/web-ui/atoms/Icons'
 import { SkipOnboardingButton } from '$/components/onboarding/skipOnboardingButton'
+import { CSPostHogProvider } from '$/app/providers'
 
 export default async function OnboardingLayout({
   children,
@@ -10,12 +11,14 @@ export default async function OnboardingLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className='flex min-h-screen flex-col max-w-[768px] pt-12 pb-4 m-auto'>
-      <div className='flex items-center justify-center'>
-        <Icon name='logo' size='large' />
+    <CSPostHogProvider>
+      <div className='flex min-h-screen flex-col max-w-[768px] pt-12 pb-4 m-auto'>
+        <div className='flex items-center justify-center'>
+          <Icon name='logo' size='large' />
+        </div>
+        {children}
+        <SkipOnboardingButton />
       </div>
-      {children}
-      <SkipOnboardingButton />
-    </div>
+    </CSPostHogProvider>
   )
 }
