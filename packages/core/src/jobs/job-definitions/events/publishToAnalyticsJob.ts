1fde25051bf069fee3a3fac0f810d9183aa8c2a6
Andrés <andresgutgon@gmail.com>
2025-05-09 10:10:33 +0200

    Implement analytics service (#1236)
    We want to have a telemetry / analytics job that track events done in
    all Latitude installations. That can be at this moment `cloud` and
    `oss`. The `oss` version has an opt out environment so this events are
    not sent to Latitude analytics.

--- packages/core/src/jobs/job-definitions/events/publishToAnalyticsJob.ts
+++ packages/core/src/jobs/job-definitions/events/publishToAnalyticsJob.ts
@@ -2,34 +2,21 @@ import { env } from '@latitude-data/env'
 import { Job } from 'bullmq'
 
 import { LatitudeEvent } from '../../../events/events'
-import { PostHogClient } from '../../../services/posthog'
+import { AnalyticsClient } from '../../../lib/analytics/AnalyticsClient'
+import { PosthogProvider } from '../../../lib/analytics/providers/Posthog'
 
 export const publishToAnalyticsJob = async (job: Job<LatitudeEvent>) => {
   const event = job.data
-  let userEmail, workspaceId
-  if ('userEmail' in event.data) {
-    userEmail = event.data.userEmail
-  }
-  if ('workspaceId' in event.data) {
-    workspaceId = event.data.workspaceId
-  }
-
-  if (!userEmail) return
-  if (env.NODE_ENV === 'development') {
-    console.log('Analytics event captured:', event.type)
-  }
-
-  const client = PostHogClient()
-  if (!client) return
-
-  client.capture({
-    distinctId: userEmail,
-    event: event.type,
-    properties: {
-      data: event.data,
-      workspaceId,
+  const client = new AnalyticsClient({
+    provider: new PosthogProvider(),
+    event,
+    env: {
+      nodeEnv: env.NODE_ENV,
+      appDomain: env.APP_DOMAIN,
+      isCloud: env.LATITUDE_CLOUD,
+      optOutAnalytics: env.OPT_OUT_ANALYTICS,
     },
   })
 
-  await client.shutdown()
+  await client.capture()
 }
