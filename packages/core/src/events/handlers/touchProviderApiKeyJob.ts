659afb6626fc3c448131604a4947776d2e95c208
Gerard Clos <clos.gerard@gmail.com>
2025-05-28 18:59:32 +0200

    tmp: disable touch api key job
    We want to process this job in queue as fast as possible in order to
    avoid a production bottleneck in Latitude Cloud background jobs queue.

--- packages/core/src/events/handlers/touchProviderApiKeyJob.ts
+++ packages/core/src/events/handlers/touchProviderApiKeyJob.ts
@@ -1,15 +1,19 @@
-import { ProviderLogsRepository } from '../../repositories'
-import { touchProviderApiKey } from '../../services/providerApiKeys/touch'
+//import { ProviderLogsRepository } from '../../repositories'
+//import { touchProviderApiKey } from '../../services/providerApiKeys/touch'
 import { EventHandler, ProviderLogCreatedEvent } from '../events'
 
 export const touchProviderApiKeyJob: EventHandler<
   ProviderLogCreatedEvent
-> = async ({ data: event }: { data: ProviderLogCreatedEvent }) => {
-  const { id, workspaceId } = event.data
-  const repo = new ProviderLogsRepository(workspaceId)
-  const providerLog = await repo.find(id).then((r) => r.unwrap())
+> = async () => {
+  return
 
-  if (providerLog.providerId) {
-    await touchProviderApiKey(providerLog.providerId).then((r) => r.unwrap())
-  }
+  // NOTE: this is temporary
+  //
+  //const { id, workspaceId } = event.data
+  //const repo = new ProviderLogsRepository(workspaceId)
+  //const providerLog = await repo.find(id).then((r) => r.unwrap())
+  //
+  //if (providerLog.providerId) {
+  //  await touchProviderApiKey(providerLog.providerId).then((r) => r.unwrap())
+  //}
 }
