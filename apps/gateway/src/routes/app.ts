197e6b9429ce0f55c63ee0f7f2eff56a2704ead1
Gerard Clos <clos.gerard@gmail.com>
2025-06-02 13:41:32 +0200

    tmp: remove tracer

--- apps/gateway/src/routes/app.ts
+++ apps/gateway/src/routes/app.ts
@@ -8,15 +8,12 @@ import createApp from '$/openApi/createApp'
 import configureOpenAPI from '$/openApi/configureOpenAPI'
 import { configureApiRoutes } from './api'
 import { configureWebhookRoutes } from './webhook'
-import { tracerMiddleware } from '$/middlewares/tracer'
 
 const app = createApp()
 
 if (process.env.NODE_ENV !== 'test') {
   app.use(logger())
 }
-
-app.use(tracerMiddleware())
 
 app.get('/health', (c) => {
   return c.json({ status: 'ok' })
