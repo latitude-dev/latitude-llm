3be425b75e7cbfbd6864e8632eea9224fcd16cc7
Gerard Clos <clos.gerard@gmail.com>
2025-06-02 22:19:32 +0200

    chore: add memory logging to gateway

--- apps/gateway/src/routes/app.ts
+++ apps/gateway/src/routes/app.ts
@@ -8,12 +8,14 @@ import createApp from '$/openApi/createApp'
 import configureOpenAPI from '$/openApi/configureOpenAPI'
 import { configureApiRoutes } from './api'
 import { configureWebhookRoutes } from './webhook'
+import { memoryUsageMiddleware } from '$/middlewares/memoryLogger'
 import { tracerMiddleware } from '$/middlewares/tracer'
 
 const app = createApp()
 
 if (process.env.NODE_ENV !== 'test') {
   app.use(logger())
+  app.use(memoryUsageMiddleware())
 }
 
 app.use(tracerMiddleware())
