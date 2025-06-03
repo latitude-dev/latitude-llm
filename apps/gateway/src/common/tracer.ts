1d3a7b24673bca4056af95ba327ffe7ab9308be1
Gerard Clos <clos.gerard@gmail.com>
2025-06-03 16:28:48 +0200

    chore: force profiling and apm in production
    For some reason profiling is not enabled in production even though the
    env var is correctly set.

--- apps/gateway/src/common/tracer.ts
+++ apps/gateway/src/common/tracer.ts
@@ -1,6 +1,8 @@
 import tracer from 'dd-trace'
 
 tracer.init({
+  profiling: process.env.NODE_ENV === 'production',
+  apmTracingEnabled: process.env.NODE_ENV === 'production',
   service: 'latitude-llm-gateway',
   env: process.env.NODE_ENV,
 })
