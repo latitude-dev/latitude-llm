1a73f871675ed8ba93ece89c445902360a846ee3
andresgutgon <andresgutgon@gmail.com>
2025-06-20 12:49:35 +0200

    feat: wip

--- packages/web-ui/src/ds/molecules/BlocksEditor/Editor/extensions/MessageReference/View.tsx
+++ packages/web-ui/src/ds/molecules/BlocksEditor/Editor/extensions/MessageReference/View.tsx
@@ -5,29 +5,26 @@ import {
   TypedNodeViewProps,
   withNodeViewProps,
 } from '../../BaseNodeView'
-import { Text } from '../../../../../atoms/Text'
-import { cn } from '../../../../../../lib/utils'
-
-type RoleColor = { bar: string }
-const COLOR_BY_ROLE: Record<MessageBlockType, RoleColor> = {
-  system: { bar: 'bg-blue-400' },
-  assistant: { bar: 'bg-emerald-500' },
-  developer: { bar: 'bg-purple-500' },
-  user: { bar: 'bg-gray-400' },
-}
+import { Badge } from '../../../../../atoms/Badge'
+import { roleToString, roleVariant } from '../../../../ChatWrapper'
 
 type Props = TypedNodeViewProps<{ role: MessageBlockType }>
 export type Attr = Props['node']['attrs']
 
 function View({ node }: Props) {
+  const role = node.attrs.role
+  const label = roleToString(role)
+  const variant = roleVariant(role)
   return (
-    <BaseNodeView className='flex flex-row gap-x-2'>
-      <div
-        className={cn('w-1', COLOR_BY_ROLE[node.attrs.role].bar)}
-      />
-      <div className='flex-1 flex flex-col gap-y-2'>
-        <Text.H5M uppercase>{node.attrs.role}</Text.H5M>
-        <NodeViewContent className='base-node-view' />
+    <BaseNodeView className='flex flex-col gap-1 w-full items-start'>
+      <div>
+        <Badge variant={variant}>{label}</Badge>
+      </div>
+      <div className='flex w-full flex-row items-stretch gap-4 pl-4'>
+        <div className='flex-shrink-0 bg-muted w-1 rounded-lg cursor-pointer transition-colors' />
+        <div className='flex flex-grow flex-col gap-1 overflow-x-auto'>
+          <NodeViewContent className='base-node-view' />
+        </div>
       </div>
     </BaseNodeView>
   )
