9dea0a2f6ce6b54ec62d272c2d3e84fee9262065
Gerard <clos.gerard@gmail.com>
2025-07-10 12:08:34 +0200

    tmp: comment out span ingestion

--- apps/gateway/src/routes/api/v3/traces/ingest/ingest.handler.ts
+++ apps/gateway/src/routes/api/v3/traces/ingest/ingest.handler.ts
@@ -1,22 +1,21 @@
 import { AppRouteHandler } from '$/openApi/types'
-import { TRACING_JOBS_MAX_ATTEMPTS } from '@latitude-data/core/browser'
-import { tracingQueue } from '@latitude-data/core/queues'
 import { IngestRoute } from './ingest.route'
 
 export const ingestHandler: AppRouteHandler<IngestRoute> = async (ctx) => {
-  const workspace = ctx.get('workspace')
-  const apiKey = ctx.get('apiKey')
-  const request = ctx.req.valid('json')
-
-  await tracingQueue.add(
-    'ingestSpansJob',
-    {
-      spans: request.resourceSpans,
-      apiKeyId: apiKey.id,
-      workspaceId: workspace.id,
-    },
-    { attempts: TRACING_JOBS_MAX_ATTEMPTS },
-  )
+  //const workspace = ctx.get('workspace')
+  //const apiKey = ctx.get('apiKey')
+  //const request = ctx.req.valid('json')
+  //
+  // TODO: Causing too much strain in background job processing. Uncomment when fixed.
+  //await tracingQueue.add(
+  //  'ingestSpansJob',
+  //  {
+  //    spans: request.resourceSpans,
+  //    apiKeyId: apiKey.id,
+  //    workspaceId: workspace.id,
+  //  },
+  //  { attempts: TRACING_JOBS_MAX_ATTEMPTS },
+  //)
 
   return ctx.body(null, 200)
 }
