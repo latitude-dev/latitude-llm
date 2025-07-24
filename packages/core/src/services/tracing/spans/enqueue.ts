77a5acd5a94022853eb0c45d822dcf202d095a5d
Gerard <clos.gerard@gmail.com>
2025-07-24 12:40:37 +0200

    tmp(tracing): disable queue

--- packages/core/src/services/tracing/spans/enqueue.ts
+++ packages/core/src/services/tracing/spans/enqueue.ts
@@ -1,74 +1,16 @@
-import {
-  ApiKey,
-  Otlp,
-  SPAN_INGESTION_STORAGE_KEY,
-  SpanIngestionData,
-  TRACING_JOBS_MAX_ATTEMPTS,
-  Workspace,
-} from '../../../browser'
-import { ingestSpansJobKey } from '../../../jobs/job-definitions/tracing/ingestSpansJob'
-import { tracingQueue } from '../../../jobs/queues'
+import { ApiKey, Otlp, Workspace } from '../../../browser'
 import { diskFactory, DiskWrapper } from '../../../lib/disk'
-import { hashContent as hash } from '../../../lib/hashContent'
-import { Result, TypedResult } from '../../../lib/Result'
+import { Result } from '../../../lib/Result'
 
 export async function enqueueSpans(
-  {
-    spans,
-    apiKey,
-    workspace,
-  }: {
+  _: {
     spans: Otlp.ResourceSpan[]
     apiKey?: ApiKey
     workspace?: Workspace
   },
-  disk: DiskWrapper = diskFactory('private'),
+  __: DiskWrapper = diskFactory('private'),
 ) {
-  const ingestionId = await generateIngestionId(spans)
-  const uploadResult = await uploadSpansToStorage(ingestionId, spans, disk)
-  if (uploadResult.error) return uploadResult
-
-  const payload = {
-    ingestionId: ingestionId,
-    apiKeyId: apiKey?.id,
-    workspaceId: workspace?.id,
-  }
-
-  await tracingQueue.add('ingestSpansJob', payload, {
-    attempts: TRACING_JOBS_MAX_ATTEMPTS,
-    deduplication: { id: ingestSpansJobKey(payload) },
-  })
+  // TODO(gerard): Recove this
 
   return Result.nil()
-}
-
-async function uploadSpansToStorage(
-  ingestionId: string,
-  spans: Otlp.ResourceSpan[],
-  disk: DiskWrapper,
-): Promise<TypedResult<void, Error>> {
-  const key = SPAN_INGESTION_STORAGE_KEY(ingestionId)
-  const data = { ingestionId, spans } satisfies SpanIngestionData
-
-  try {
-    const payload = JSON.stringify(data)
-    await disk.put(key, payload).then((r) => r.unwrap())
-    return Result.nil()
-  } catch (error) {
-    return Result.error(error as Error)
-  }
-}
-
-async function generateIngestionId(
-  spans: Otlp.ResourceSpan[],
-): Promise<string> {
-  let ingestionId = ''
-  for (const { scopeSpans } of spans) {
-    for (const { spans } of scopeSpans) {
-      for (const span of spans) {
-        ingestionId += span.traceId + span.spanId
-      }
-    }
-  }
-  return hash(ingestionId)
 }
