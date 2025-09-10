f661b823b2fba9114ea7f3a17fa2dcfccd688c45
mnavid <learningbizz@gmail.com>
2025-09-10 15:16:41 +0200

    wip

---
+++ apps/web/src/actions/admin/promocodes/createPromocode.ts
@@ -0,0 +1,27 @@
+'use server'
+
+import { z } from 'zod'
+import { withAdmin } from '../../procedures'
+import { createPromocode } from '@latitude-data/core/services/promocodes/create'
+import { QuotaType } from '@latitude-data/constants'
+
+export const createPromocodeAction = withAdmin
+  .createServerAction()
+  .input(
+    z.object({
+      code: z.string(),
+      quotaType: z.nativeEnum(QuotaType),
+      description: z.string(),
+      amount: z.number(),
+    }),
+  )
+  .handler(async ({ input }) => {
+    const createdPromocodeResult = await createPromocode({
+      code: input.code,
+      quotaType: input.quotaType,
+      description: input.description,
+      amount: input.amount,
+    })
+
+    return createdPromocodeResult.unwrap()
+  })
