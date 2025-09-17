b9f0367379de9b209005988399cb469da86de76f
Alex Rodríguez <me@arn.sh>
2025-09-17 18:28:30 +0200

    Fix v2 plans

--- packages/core/src/plans.ts
+++ packages/core/src/plans.ts
@@ -10,18 +10,9 @@ export enum SubscriptionPlan {
 const NO_STRIPE_PRICE = 'NO_STRIPE_PRICE'
 
 export const SubscriptionPlans = {
-  [SubscriptionPlan.HobbyV2]: {
-    name: 'Hobby',
-    credits: 10_000,
-    users: 1,
-    retention_period: 30, // days
-    rate_limit: 10, // per second
-    latte_credits: 30,
-    stripePriceId: NO_STRIPE_PRICE,
-  },
   [SubscriptionPlan.HobbyV1]: {
     name: 'Hobby',
-    credits: 50_000,
+    credits: 50_000, // runs
     users: 1,
     retention_period: 30, // days
     rate_limit: 10, // per second
@@ -30,35 +21,44 @@ export const SubscriptionPlans = {
   },
   [SubscriptionPlan.TeamV1]: {
     name: 'Team',
-    credits: 100_000,
+    credits: 100_000, // runs
     users: 5,
     retention_period: 1825, // days
     rate_limit: 166, // per second
     latte_credits: 300,
     stripePriceId: 'price_1QjVaoAMdFMjIC4f7oRcoEzE',
   },
+  [SubscriptionPlan.HobbyV2]: {
+    name: 'Hobby',
+    credits: 10_000, // runs
+    users: 1,
+    retention_period: 30, // days
+    rate_limit: 10, // per second
+    latte_credits: 30,
+    stripePriceId: NO_STRIPE_PRICE,
+  },
   [SubscriptionPlan.ProV2]: {
     name: 'Pro',
+    credits: 80_000, // runs
     users: 1,
     retention_period: 30, // days
     rate_limit: 83, // per second
-    credits: 30_000,
     latte_credits: 100,
     stripePriceId: 'price_1S5XYVAMdFMjIC4ff8jaaa7B',
   },
   [SubscriptionPlan.TeamV2]: {
     name: 'Team',
+    credits: 200_000, // runs
     users: 5,
     retention_period: 1825, // days
     rate_limit: 166, // per second
-    credits: 100_000,
     latte_credits: 300,
     stripePriceId: 'price_1S5XcmAMdFMjIC4fco75QEuP',
   },
   [SubscriptionPlan.EnterpriseV1]: {
     name: 'Enterprise',
-    credits: 1_000_000,
-    users: 1000,
+    credits: 'unlimited' as const, // runs
+    users: 'unlimited' as const,
     retention_period: 3650, // days
     rate_limit: 500, // per second
     latte_credits: 'unlimited' as const,
