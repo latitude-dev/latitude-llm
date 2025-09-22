0f4c52584348fa341b3d6fa31222e39419e2fb6c
Gerard <clos.gerard@gmail.com>
2025-09-22 12:07:14 +0200

    chore: increase maintenance queue concurrency

--- apps/workers/src/workers/worker-definitions/maintenanceWorker.ts
+++ apps/workers/src/workers/worker-definitions/maintenanceWorker.ts
@@ -2,6 +2,7 @@ import { setupLRO } from '@latitude-data/core/client'
 import * as jobs from '@latitude-data/core/jobs/definitions'
 import { Queues } from '@latitude-data/core/queues/types'
 import { createWorker } from '../utils/createWorker'
+import { WORKER_CONNECTION_CONFIG } from '../utils/connectionConfig'
 
 const jobMappings = {
   autoScaleJob: jobs.autoScaleJob,
@@ -29,5 +30,9 @@ const jobMappings = {
 
 export function startMaintenanceWorker() {
   setupLRO() // Setup LRO for the maintenance worker
-  return createWorker(Queues.maintenanceQueue, jobMappings)
+
+  return createWorker(Queues.maintenanceQueue, jobMappings, {
+    concurrency: 25,
+    connection: WORKER_CONNECTION_CONFIG,
+  })
 }
