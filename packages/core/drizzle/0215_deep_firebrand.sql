c3fa16e1c370e16450047f5a929f8c89f5296cd5
Gerard <clos.gerard@gmail.com>
2025-10-01 14:13:24 +0200

    chore: create indices concurrently

--- packages/core/drizzle/0215_deep_firebrand.sql
+++ packages/core/drizzle/0215_deep_firebrand.sql
@@ -1,6 +1,8 @@
 ALTER TABLE "latitude"."spans" ADD COLUMN "document_uuid" uuid;--> statement-breakpoint
 ALTER TABLE "latitude"."spans" ADD COLUMN "commit_uuid" uuid;--> statement-breakpoint
 ALTER TABLE "latitude"."spans" ADD COLUMN "experiment_id" bigint;--> statement-breakpoint
-CREATE INDEX "spans_document_uuid_idx" ON "latitude"."spans" USING btree ("document_uuid");--> statement-breakpoint
-CREATE INDEX "spans_commit_uuid_idx" ON "latitude"."spans" USING btree ("commit_uuid");--> statement-breakpoint
-CREATE INDEX "spans_experiment_id_idx" ON "latitude"."spans" USING btree ("experiment_id");
+COMMIT;
+
+CREATE INDEX CONCURRENTLY "spans_document_uuid_idx" ON "latitude"."spans" USING btree ("document_uuid");--> statement-breakpoint
+CREATE INDEX CONCURRENTLY "spans_commit_uuid_idx" ON "latitude"."spans" USING btree ("commit_uuid");--> statement-breakpoint
+CREATE INDEX CONCURRENTLY "spans_experiment_id_idx" ON "latitude"."spans" USING btree ("experiment_id");
