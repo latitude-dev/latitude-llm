7043b43142a78bb2fb66bf58202b287049e3ad95
Gerard <clos.gerard@gmail.com>
2025-10-01 14:23:11 +0200

    chore: fix migration (take #3)

--- packages/core/drizzle/0215_deep_firebrand.sql
+++ packages/core/drizzle/0215_deep_firebrand.sql
@@ -1,7 +1,12 @@
+COMMIT;
+--> statement-breakpoint
+
+BEGIN;
 ALTER TABLE "latitude"."spans" ADD COLUMN "document_uuid" uuid;--> statement-breakpoint
 ALTER TABLE "latitude"."spans" ADD COLUMN "commit_uuid" uuid;--> statement-breakpoint
 ALTER TABLE "latitude"."spans" ADD COLUMN "experiment_id" bigint;--> statement-breakpoint
 COMMIT;
+--> statement-breakpoint
 
 CREATE INDEX CONCURRENTLY "spans_document_uuid_idx" ON "latitude"."spans" USING btree ("document_uuid");--> statement-breakpoint
 CREATE INDEX CONCURRENTLY "spans_commit_uuid_idx" ON "latitude"."spans" USING btree ("commit_uuid");--> statement-breakpoint
