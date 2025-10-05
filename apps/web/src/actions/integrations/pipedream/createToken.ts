984134f618249bcad17882758fec773c9a30ccdc
Gerard <clos.gerard@gmail.com>
2025-10-05 09:31:06 +0200

    fix tc for realz

--- apps/web/src/actions/integrations/pipedream/createToken.ts
+++ apps/web/src/actions/integrations/pipedream/createToken.ts
@@ -2,15 +2,9 @@
 
 import { authProcedure } from '../../procedures'
 import { createConnectToken } from '@latitude-data/core/services/integrations/pipedream/createConnectToken'
-import { z } from 'zod'
 
 export const createPipedreamTokenAction = authProcedure
   .createServerAction()
-  .input(
-    z.object({
-      externalUserId: z.string(),
-    }),
-  )
   .handler(async ({ ctx }) =>
     createConnectToken({
       workspace: ctx.workspace,
