0c057138f161f3d7293b8cbb778fa19a8c3d704b
mnavid <learningbizz@gmail.com>
2025-10-14 11:59:56 +0200

    Fix job to insert new workspaceOnboardings

--- packages/core/src/jobs/job-definitions/maintenance/fillOnboardingsAsCompletedJob.ts
+++ packages/core/src/jobs/job-definitions/maintenance/fillOnboardingsAsCompletedJob.ts
@@ -2,23 +2,42 @@ import { Job } from 'bullmq'
 import { Result } from '../../../lib/Result'
 import { workspaceOnboarding } from '../../../schema/models/workspaceOnboarding'
 import { database } from '../../../client'
-import { isNull } from 'drizzle-orm'
+import { eq, notExists } from 'drizzle-orm'
+import { workspaces } from '../../../schema/models/workspaces'
 
 export type FillOnboardingsAsCompletedJobData = Record<string, never>
 
 export const fillOnboardingsAsCompletedJob = async (
   _: Job<FillOnboardingsAsCompletedJobData>,
 ) => {
-  const updatedOnboardings = await database
-    .update(workspaceOnboarding)
-    .set({
-      completedAt: new Date(),
+  const onboardingsToInsert = await database
+    .select({
+      workspaceId: workspaces.id,
     })
-    .where(isNull(workspaceOnboarding.completedAt))
-    .returning()
+    .from(workspaces)
+    .where(
+      notExists(
+        database
+          .select()
+          .from(workspaceOnboarding)
+          .where(eq(workspaceOnboarding.workspaceId, workspaces.id)),
+      ),
+    )
+
+  const completedOnboardingsCreated = await database
+    .insert(workspaceOnboarding)
+    .values(
+      onboardingsToInsert.map(({ workspaceId }) => ({
+        workspaceId,
+        completedAt: new Date(),
+      })),
+    )
+    .returning({
+      workspaceId: workspaceOnboarding.workspaceId,
+    })
 
   return Result.ok({
-    success: true,
-    updatedOnboardings: updatedOnboardings.length,
+    success: completedOnboardingsCreated.length === onboardingsToInsert.length,
+    completedOnboardings: completedOnboardingsCreated.length,
   })
 }
