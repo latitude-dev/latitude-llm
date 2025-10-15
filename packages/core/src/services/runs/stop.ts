496fd15957987be483c62fd759e7054eb6609e94
Gerard <clos.gerard@gmail.com>
2025-10-15 11:41:46 +0200

    fix(core): clean active run if no job is found
    The active runs and job state can get desynced in unexpected conditions.
    The stop service should clean up the state in that case.
    
    Added tests.

--- packages/core/src/services/runs/stop.ts
+++ packages/core/src/services/runs/stop.ts
@@ -1,13 +1,16 @@
 import { Run } from '../../constants'
 import { publisher } from '../../events/publisher'
 import { queues } from '../../jobs/queues'
-import { NotFoundError, UnprocessableEntityError } from '../../lib/errors'
+import { UnprocessableEntityError } from '../../lib/errors'
 import { Result } from '../../lib/Result'
+import { RunsRepository } from '../../repositories'
 import { Project, Workspace } from '../../schema/types'
 import { JOB_FINISHED_STATES, subscribeQueue } from './shared'
 
 export async function stopRun({
   run,
+  project,
+  workspace,
 }: {
   run: Run
   project: Project
@@ -20,13 +23,21 @@ export async function stopRun({
   const { runsQueue } = await queues()
   const job = await runsQueue.getJob(run.uuid)
   if (!job?.id) {
-    return Result.error(
-      new NotFoundError(`Active run job with uuid ${run.uuid} not found`),
-    )
+    const runsRepo = new RunsRepository(workspace.id, project.id)
+    const result = await runsRepo.delete({ runUuid: run.uuid })
+    if (!Result.isOk(result)) return result
+
+    return Result.nil()
   }
 
-  const state = await job.getState()
-  if (!JOB_FINISHED_STATES.includes(state)) {
+  let state: string | undefined
+  try {
+    state = await job.getState()
+  } catch {
+    /* No-op */
+  }
+
+  if (state && !JOB_FINISHED_STATES.includes(state)) {
     publisher.publish('cancelJob', { jobId: job.id })
 
     try {
