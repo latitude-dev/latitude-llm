1c4775b50943bba8e54c7eb6a9559d773e4b91f0
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2025-10-27 11:15:12 +0100

    Simulation Settings in Document Logs

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -62,6 +62,11 @@ export type ExperimentScores = {
   [evaluationUuid: string]: ExperimentEvaluationScore
 }
 
+export type SimulationSettings = {
+  simulationDescription?: string // A prompt used to guide and generate the simulation result
+  simulateToolResponses?: boolean
+}
+
 // TODO: Remove these
 export * from './ai'
 export * from './config'
