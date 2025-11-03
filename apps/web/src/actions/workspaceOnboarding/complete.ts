2da730ff464fbe37167a46423e10c7dc324324cc
mnavid <learningbizz@gmail.com>
2025-11-03 14:32:31 +0100

    remove more

--- apps/web/src/actions/workspaceOnboarding/complete.ts
+++ apps/web/src/actions/workspaceOnboarding/complete.ts
@@ -6,9 +6,7 @@ import { authProcedure } from '../procedures'
 import { frontendRedirect } from '$/lib/frontendRedirect'
 import { ROUTES } from '$/services/routes'
 import { z } from 'zod'
-import { isFeatureEnabledByName } from '@latitude-data/core/services/workspaceFeatures/isFeatureEnabledByName'
-import { Result } from '@latitude-data/core/lib/Result'
-import { RunSourceGroup } from '@latitude-data/core/constants'
+import { RunSourceGroup } from '@latitude-data/constants'
 
 /**
  * Mark onboarding as complete
@@ -18,12 +16,10 @@ export const completeOnboardingAction = authProcedure
     z.object({
       projectId: z.number(),
       commitUuid: z.string(),
-      documentUuid: z.string().optional(),
-      experimentUuids: z.array(z.string()).optional(),
     }),
   )
   .action(async ({ parsedInput, ctx }) => {
-    const { projectId, commitUuid, documentUuid, experimentUuids } = parsedInput
+    const { projectId, commitUuid } = parsedInput
     const onboarding = await getWorkspaceOnboarding({
       workspace: ctx.workspace,
     }).then((r) => r.unwrap())
@@ -32,42 +28,12 @@ export const completeOnboardingAction = authProcedure
       onboarding,
     }).then((r) => r.unwrap())
 
-    const isExperimentsOnboarding = documentUuid && experimentUuids
-    if (isExperimentsOnboarding) {
-      return frontendRedirect(
-        ROUTES.projects
-          .detail({ id: projectId })
-          .commits.detail({ uuid: commitUuid })
-          .documents.detail({ uuid: documentUuid })
-          .experiments.withSelected(experimentUuids),
-      )
-    }
-
-    const isDatasetOnboardingEnabledResult = await isFeatureEnabledByName(
-      ctx.workspace.id,
-      'datasetOnboarding',
-    )
-
-    if (!Result.isOk(isDatasetOnboardingEnabledResult)) {
-      return frontendRedirect(ROUTES.dashboard.root)
-    }
-    const isDatasetOnboardingEnabled = isDatasetOnboardingEnabledResult.unwrap()
-
-    if (isDatasetOnboardingEnabled) {
-      return frontendRedirect(
-        ROUTES.projects
-          .detail({ id: projectId })
-          .commits.detail({ uuid: commitUuid })
-          .runs.root({
-            sourceGroup: RunSourceGroup.Playground,
-          }),
-      )
-    }
-
-    // Default redirect to the project's home page
     return frontendRedirect(
       ROUTES.projects
         .detail({ id: projectId })
-        .commits.detail({ uuid: commitUuid }).home.root,
+        .commits.detail({ uuid: commitUuid })
+        .runs.root({
+          sourceGroup: RunSourceGroup.Playground,
+        }),
     )
   })
