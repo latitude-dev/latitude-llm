df953611eed39f841e56884e09bc00e7afda3514
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2025-11-17 13:01:46 +0100

    New Inter Display font for larger Heading texts (#1941)

--- apps/web/src/helpers/fonts.ts
+++ apps/web/src/helpers/fonts.ts
@@ -76,6 +76,82 @@ export const fontSans = localFont({
   variable: '--font-sans',
 })
 
+export const fontDisplay = localFont({
+  src: [
+    {
+      path: '../assets/fonts/InterDisplay-ExtraLight.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-ExtraLightItalic.woff2',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-LightItalic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-Italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-MediumItalic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-SemiBoldItalic.woff2',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-Black.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/InterDisplay-BlackItalic.woff2',
+      weight: '800',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-display',
+})
+
 export const fontMono = localFont({
   src: [
     {
