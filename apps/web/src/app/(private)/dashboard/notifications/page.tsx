8987664afe59e1b24f170f56529375a1c424c682
andresgutgon <andresgutgon@gmail.com>
2025-11-21 19:18:03 +0100

    WIP

--- apps/web/src/app/(private)/dashboard/notifications/page.tsx
+++ apps/web/src/app/(private)/dashboard/notifications/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next'
 import NotificationsModal from '$/components/Notifications/Modal'
-import { metadata as modalMetadata } from '../../@modal/(.)notifications/page'
+import { metadata as modalMetadata } from '../../notifications/page'
 import { ROUTES } from '$/services/routes'
 
 export const metadata: Promise<Metadata> = modalMetadata
