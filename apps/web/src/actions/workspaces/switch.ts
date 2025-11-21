927b8ca0f664cf89bcb84069a6d4333f7bea506e
andresgutgon <andresgutgon@gmail.com>
2025-11-21 19:10:22 +0100

    WIP

--- apps/web/src/actions/workspaces/switch.ts
+++ apps/web/src/actions/workspaces/switch.ts
@@ -7,7 +7,6 @@ import { setSession } from '$/services/auth/setSession'
 import { authProcedure } from '../procedures'
 import { cookies } from 'next/headers'
 import { removeSession, Session } from '$/services/auth/removeSession'
-import { ROUTES } from '$/services/routes'
 import { redirect } from 'next/navigation'
 
 export const switchWorkspaceAction = authProcedure
@@ -40,5 +39,9 @@ export const switchWorkspaceAction = authProcedure
       await cookies(),
     )
 
-    return redirect(parsedInput.redirectTo ?? ROUTES.root)
+    if (parsedInput.redirectTo) {
+      return redirect(parsedInput.redirectTo)
+    }
+
+    return workspace
   })
