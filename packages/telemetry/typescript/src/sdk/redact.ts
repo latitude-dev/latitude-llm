37a62b507a7f0f0cfcadb9def497921d5ec146f7
Gerard Clos <clos.gerard@gmail.com>
2025-12-15 15:06:07 +0100

    quickfix: do not redact token usage from spans

--- packages/telemetry/typescript/src/sdk/redact.ts
+++ packages/telemetry/typescript/src/sdk/redact.ts
@@ -70,7 +70,7 @@ export const DEFAULT_REDACT_SPAN_PROCESSOR = () =>
     attributes: [
       /^.*auth.*$/i,
       /^.*authorization.*$/i,
-      /^(?!ai\.).*usage.*$/i,
+      /^(?!gen_ai\.).*usage.*$/i,
       /^(?!gen_ai\.).*token.*$/i,
       /^.*secret.*$/i,
       /^.*key.*$/i,
