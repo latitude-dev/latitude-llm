2e393abe06f7781149badc4f6de31ceda49c7ed4
mnavid <learningbizz@gmail.com>
2026-01-05 17:15:53 +0100

    fix migration

--- packages/core/drizzle/0253_add_eval_result_type_and_metric.sql
+++ packages/core/drizzle/0253_add_eval_result_type_and_metric.sql
@@ -1,6 +1,10 @@
+COMMIT;
+--> statement-breakpoint
+
 BEGIN;
 ALTER TABLE "latitude"."evaluation_results_v2" ADD COLUMN "type" varchar(32) NOT NULL;--> statement-breakpoint
 ALTER TABLE "latitude"."evaluation_results_v2" ADD COLUMN "metric" varchar(64) NOT NULL;--> statement-breakpoint
 COMMIT;
+--> statement-breakpoint
 
 CREATE INDEX CONCURRENTLY "evaluation_results_v2_type_workspace_idx" ON "latitude"."evaluation_results_v2" USING btree ("type","workspace_id");
