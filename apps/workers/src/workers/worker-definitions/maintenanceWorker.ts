0b03a88b5ff41fd77ca42d4cdbaeecd34f21c3fa
mnavid <learningbizz@gmail.com>
2026-01-06 12:25:51 +0100

    Add more concurrency to maintenance workers for backfill evalResults

--- apps/workers/src/workers/worker-definitions/maintenanceWorker.ts
+++ apps/workers/src/workers/worker-definitions/maintenanceWorker.ts
@@ -2,6 +2,7 @@ import { setupLRO } from '@latitude-data/core/client'
 import * as jobs from '@latitude-data/core/jobs/definitions'
 import { Queues } from '@latitude-data/core/queues/types'
 import { createWorker } from '../utils/createWorker'
+import { WORKER_CONNECTION_CONFIG } from '../utils/connectionConfig'
 
 const jobMappings = {
   autoScaleJob: jobs.autoScaleJob,
@@ -23,5 +24,8 @@ const jobMappings = {
 export function startMaintenanceWorker() {
   setupLRO() // Setup LRO for the maintenance worker
 
-  return createWorker(Queues.maintenanceQueue, jobMappings)
+  return createWorker(Queues.maintenanceQueue, jobMappings, {
+    connection: WORKER_CONNECTION_CONFIG,
+    concurrency: 20,
+  })
 }
