cf66e084c886480144ef84554db4cabe7fe09931
Gerard <clos.gerard@gmail.com>
2026-01-15 11:27:22 +0100

    minor: update alert message

--- apps/web/src/components/IntegrationGallery/InstallationSteps/index.tsx
+++ apps/web/src/components/IntegrationGallery/InstallationSteps/index.tsx
@@ -252,7 +252,7 @@ LATITUDE_PROJECT_ID=${projectId}
         description='Initialize Latitude Telemetry and wrap the code that generates the AI response using telemetry.capture. You must add a path to identify this prompt in your Latitude project.'
       >
         <>
-          <Alert description='The path is used to identify the prompt in your Latitude project. It must not contain spaces or special characters (use letters, numbers, - _ / .), and it will automatically create a new prompt in your Latitude project if it does not exist.' />
+          <Alert description='The path must not contain spaces or special characters (use letters, numbers, - _ / .)' />
           {pythonOnlyMode ? (
             <CodeBlock language='python' textWrap={false}>
               {implementationCodeblockPython(framework)}
