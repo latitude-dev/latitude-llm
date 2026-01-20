74e0c42b6b3feda1cc69360dcb123b2db29ad405
César Migueláñez <cesar@cesar.wtf>
2026-01-20 16:28:15 +0100

    fix: use correct GTM snippet (#2148)
    - Replace gtag.js with proper GTM container snippet
    - Add noscript fallback iframe

--- apps/web/src/components/Providers/GoogleTagManager/index.tsx
+++ apps/web/src/components/Providers/GoogleTagManager/index.tsx
@@ -10,19 +10,23 @@ export function GoogleTagManager() {
 
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`}
-        strategy='afterInteractive'
-      />
-      <Script id='gtm-init' strategy='afterInteractive'>
+      <Script id='gtm-head' strategy='afterInteractive'>
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gtmId}');
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtmId}');
         `}
       </Script>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+          height='0'
+          width='0'
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
     </>
   )
 }
-
