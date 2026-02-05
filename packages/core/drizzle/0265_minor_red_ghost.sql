f1935e8100d3c7747dd71d22c8d113c923f52be4
Gerard <clos.gerard@gmail.com>
2026-02-05 16:14:39 +0100

    rollback: migration is too expensive, skipping it

--- packages/core/drizzle/0265_minor_red_ghost.sql
+++ packages/core/drizzle/0265_minor_red_ghost.sql
@@ -1,8 +1,8 @@
-COMMIT;
---> statement-breakpoint
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS "spans_workspace_project_started_at_id_idx" ON "latitude"."spans" USING btree ("workspace_id","project_id","started_at","id");
---> statement-breakpoint
-
-CREATE INDEX CONCURRENTLY IF NOT EXISTS "spans_workspace_document_started_at_id_idx" ON "latitude"."spans" USING btree ("workspace_id","document_uuid","started_at","id");
-
+-- COMMIT;
+-- --> statement-breakpoint
+--
+-- CREATE INDEX CONCURRENTLY IF NOT EXISTS "spans_workspace_project_started_at_id_idx" ON "latitude"."spans" USING btree ("workspace_id","project_id","started_at","id");
+-- --> statement-breakpoint
+--
+-- CREATE INDEX CONCURRENTLY IF NOT EXISTS "spans_workspace_document_started_at_id_idx" ON "latitude"."spans" USING btree ("workspace_id","document_uuid","started_at","id");
+--
