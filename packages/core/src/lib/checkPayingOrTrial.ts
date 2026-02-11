1b747e105f51d6d60ed838ad7f7a001349c747dc
Andrés <andresgutgon@gmail.com>
2026-02-11 09:38:18 +0100

    Enforce trial in gateway (#2258)
    Move the check to the gateway boundary

--- packages/core/src/lib/checkPayingOrTrial.ts
+++ packages/core/src/lib/checkPayingOrTrial.ts
@@ -1,17 +1,17 @@
 import { PaymentRequiredError } from '@latitude-data/constants/errors'
-import { isPayingOrTrialing } from '../../plans'
-import { Result, TypedResult } from '../../lib/Result'
-import { Subscription } from '../../schema/models/types/Subscription'
+import { isPayingOrTrialing } from '../plans'
+import { Result, TypedResult } from './Result'
+import { Subscription } from '../schema/models/types/Subscription'
 
 /**
  * Checks if the workspace subscription is either a paying plan or in an active trial.
  * Throws PaymentRequiredError if the trial has ended and no payment is set up.
  */
-export async function checkPayingOrTrial({
+export function checkPayingOrTrial({
   subscription,
 }: {
   subscription: Subscription
-}): Promise<TypedResult<void, PaymentRequiredError>> {
+}): TypedResult<void, PaymentRequiredError> {
   const canUsePaidFeatures = isPayingOrTrialing({
     plan: subscription.plan,
     trialEndsAt: subscription.trialEndsAt,
