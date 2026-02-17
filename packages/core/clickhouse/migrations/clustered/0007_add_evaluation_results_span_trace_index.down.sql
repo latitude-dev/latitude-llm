5324b19efbc98b9ef3327ed926c4f416779cec22
Gerard <clos.gerard@gmail.com>
2026-02-17 16:14:19 +0100

    add clickhouse index for evaluation span-trace lookups

---
+++ packages/core/clickhouse/migrations/clustered/0007_add_evaluation_results_span_trace_index.down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE evaluation_results ON CLUSTER default DROP INDEX IF EXISTS idx_evaluated_span_trace;
---
+++ packages/core/clickhouse/migrations/unclustered/0007_add_evaluation_results_span_trace_index.down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE evaluation_results DROP INDEX IF EXISTS idx_evaluated_span_trace;
