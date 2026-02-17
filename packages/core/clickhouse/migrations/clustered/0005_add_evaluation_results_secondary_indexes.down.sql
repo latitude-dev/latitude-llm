e420e34cb38aabd125e9f0ca0970b09c59f25dc5
Gerard <clos.gerard@gmail.com>
2026-02-17 13:49:46 +0100

    Add secondary indexes for ClickHouse evaluation_results (#2321)
    * chore: add clickhouse secondary indexes for evaluation results
    
    * chore: use composite trace/span index for eval results

---
+++ packages/core/clickhouse/migrations/clustered/0005_add_evaluation_results_secondary_indexes.down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE evaluation_results ON CLUSTER default DROP INDEX IF EXISTS idx_evaluated_trace_span;
+ALTER TABLE evaluation_results ON CLUSTER default DROP INDEX IF EXISTS idx_evaluated_trace_id;
+ALTER TABLE evaluation_results ON CLUSTER default DROP INDEX IF EXISTS idx_experiment_id;
+ALTER TABLE evaluation_results ON CLUSTER default DROP INDEX IF EXISTS idx_document_uuid;
+ALTER TABLE evaluation_results ON CLUSTER default DROP INDEX IF EXISTS idx_commit_uuid;
+ALTER TABLE evaluation_results ON CLUSTER default DROP INDEX IF EXISTS idx_uuid;
