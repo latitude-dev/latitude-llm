2dd9f4e28c5e50b6a5e6c580ef94677dc97fb506
Gerard <clos.gerard@gmail.com>
2026-02-18 13:03:56 +0100

    feat(clickhouse/migrations): add migration to drop backup tables

---
+++ packages/core/clickhouse/migrations/unclustered/0008_drop_backup_tables.down.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS spans_backup_0006 AS spans;
+CREATE TABLE IF NOT EXISTS evaluation_results_backup_0005 AS evaluation_results;
---
+++ packages/core/clickhouse/migrations/unclustered/0008_drop_backup_tables.up.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS spans_backup_0006;
+DROP TABLE IF EXISTS evaluation_results_backup_0005;
