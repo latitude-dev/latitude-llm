8752a315a6708cdc52bc1b6100671523caeca2d5
Gerard <clos.gerard@gmail.com>
2026-03-04 15:55:01 +0100

    code review

--- packages/platform/db-postgres/drizzle/20260303113537_create-latitude-schema-and-rls-functions/migration.sql
+++ packages/platform/db-postgres/drizzle/20260303113537_create-latitude-schema-and-rls-functions/migration.sql
@@ -5,9 +5,3 @@ BEGIN
   RETURN NULLIF(current_setting('app.current_organization_id', true), '');
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
-
-CREATE OR REPLACE FUNCTION get_current_user_id() RETURNS TEXT AS $$
-BEGIN
-  RETURN NULLIF(current_setting('app.current_user_id', true), '');
-END;
-$$ LANGUAGE plpgsql SECURITY DEFINER;
