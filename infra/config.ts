81ea81fe4a9697665dab3d9db626375ea147d5b4
Gerard <clos.gerard@gmail.com>
2026-06-19 12:08:10 +0200

    chore: increase web redundancy to 2

--- infra/config.ts
+++ infra/config.ts
@@ -218,7 +218,7 @@ export const productionConfig: EnvironmentConfig = {
         memory: 2048,
         port: 8080,
         healthCheckPath: "/api/health",
-        desiredCount: 1,
+        desiredCount: 2,
         minCount: 1,
         maxCount: 3,
       },
