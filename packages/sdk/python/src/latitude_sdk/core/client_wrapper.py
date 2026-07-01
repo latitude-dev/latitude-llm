87240b3a200f0dc9d6b02bdd2ccbcdc9c4a2bf7e
Gerard <clos.gerard@gmail.com>
2026-07-01 18:27:55 +0200

    Revert "fix(sdk): update generated Python client headers"
    This reverts commit fea4dbe66e7aa0cbf149d7de425d864b29e311d5.

--- packages/sdk/python/src/latitude_sdk/core/client_wrapper.py
+++ packages/sdk/python/src/latitude_sdk/core/client_wrapper.py
@@ -30,14 +30,8 @@ class BaseClientWrapper:
         self._logging = logging
 
     def get_headers(self) -> typing.Dict[str, str]:
-        import platform
-
         headers: typing.Dict[str, str] = {
-            "User-Agent": "latitude_sdk/7.0.1",
             "X-Fern-Language": "Python",
-            "X-Fern-Runtime": f"python/{platform.python_version()}",
-            "X-Fern-Platform": f"{platform.system().lower()}/{platform.release()}",
-            "X-Fern-Interpreter": platform.python_implementation(),
             **(self.get_custom_headers() or {}),
         }
         headers["Authorization"] = f"Bearer {self._get_api_key()}"
